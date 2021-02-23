0f8eb2733030be2e69839b28b11235c542749d1b
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2021-02-23 16:01:19 -0300

    upload to gitjub pages

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const crescer = document.querySelector('.crescer');
+
+$('.crescer').parallax({imageSrc: '../assets/background1.jpg'});
+$('.resultados').parallax({imageSrc: '../assets/background2.jpg'});
+$('.comunicacao').parallax({imageSrc: '../assets/background3.jpg'});
+$('.sabedoria').parallax({imageSrc: '../assets/background4.jpg'});
+$('.intro').parallax({imageSrc: '../assets/intro.jpg'});
